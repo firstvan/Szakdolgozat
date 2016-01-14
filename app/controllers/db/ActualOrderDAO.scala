e31039209b7dee2268c0903f08c581984d64cb67
Orban Istvan <firstvan@live.com>
2016-01-14 22:24:53 +0100

    Add new order completted. Wrote getOrderNum to get max orderid.

--- app/controllers/db/ActualOrderDAO.scala
+++ app/controllers/db/ActualOrderDAO.scala
@@ -1,7 +1,49 @@
 package controllers.db
 
-object ActualOrderDAO extends IActualOrderDAO{
-  override def addOrder(productNumber: String, ordered: Int, actualPrice: Int): Unit = {
+//import com.mongodb.casbah.commons.MongoDBObject
+import com.mongodb.casbah.Imports._
 
+
+object ActualOrderDAO extends IActualOrderDAO{
+
+  val format = new java.text.SimpleDateFormat("dd-MM-yyyy")
+
+  override def addOrder(orderNumber: Int, productNumber: String, ordered: Int, actualPrice: Int): Unit = {
+    ???
   }
+
+  /**
+    * This method will add a new record to orders table.
+    */
+  override def addNewOrder(salesManUserName: String, customerId : String, delivery: String): Int = {
+    val orderNum = getOrderNum()
+
+    val usr = UserDAO.getUserByUserName(salesManUserName).get._id
+
+    val newOrder = MongoDBObject(
+      "_id" -> orderNum,
+      "sales_man_id" -> usr,
+      "customer" -> customerId,
+      "date_of_take" -> format.format(new java.util.Date()),
+      "delivery_date" -> delivery
+    )
+
+    DBFactory.getCollection("orders").insert(newOrder)
+
+    return orderNum
+  }
+
+  private def getOrderNum(): Int = {
+    val query = MongoDBObject() // All documents
+    val fields = MongoDBObject("_id" -> 1) // Only return `_id`
+    val orderBy = MongoDBObject("_id" -> -1) // Order by _id descending
+
+    val item = DBFactory.getCollection("orders").findOne(query, fields, orderBy)
+
+    val id = item.get.as[Int]("_id")
+    println(id)
+
+    return id + 1
+  }
+
 }
--- app/controllers/db/IActualOrderDAO.scala
+++ app/controllers/db/IActualOrderDAO.scala
@@ -1,5 +1,11 @@
 package controllers.db
 
 trait IActualOrderDAO {
-def addOrder(productNumber: String ,ordered: Int, actualPrice: Int)
+def addOrder(orderNumber: Int, productNumber: String ,ordered: Int, actualPrice: Int)
+
+
+  /**
+    * This method will add a new record to orders table.
+    */
+  def addNewOrder(salesManUserName: String, customerId : String, delivery: String) : Int
 }
